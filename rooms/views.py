66ae00ca846ff69b39e28423953d116decf5f2d0
KIMJUNSICK <jinaganda5@gmail.com>
2019-11-16 09:18:27 +0900

    Views: Offset & Limit

--- rooms/views.py
+++ rooms/views.py
@@ -3,5 +3,5 @@ from . import models
 
 
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
+    all_rooms = models.Room.objects.all()[3:10]  # [Offset : Limit]
     return render(request, "rooms/home.html", context={"potato": all_rooms})
