ef9525a119d2bd847b07f4e35f55ce298e49a97d
KIMJUNSICK <jinaganda5@gmail.com>
2019-11-16 10:12:35 +0900

    Templates: pages list navigation

--- rooms/views.py
+++ rooms/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from . import models
+from math import ceil
 
 
 def all_rooms(request):
-    # infomation of page in request
-    # how to get page info neatly in queryset
-    #  variable for offset & limit
-    #  offset = (page-1) x 10 limit = page x 10
-    #  all_rooms = models.Room.objects.all()[offset:limit]
     PAGE_SIZE = 10
-    page = int(request.GET.get("page", 1))
+    page = request.GET.get("page", 1)
+    page = int(page or 1)  # for case that there are imcomplete page
     limit = page * PAGE_SIZE
     offset = limit - PAGE_SIZE
+    page_count = ceil(models.Room.objects.count() / PAGE_SIZE)
     all_rooms = models.Room.objects.all()[offset:limit]  # [Offset : Limit]
-    return render(request, "rooms/home.html", context={"potato": all_rooms})
+    return render(
+        request,
+        "rooms/home.html",
+        context={
+            "potato": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(1, page_count + 1),
+        },
+    )
