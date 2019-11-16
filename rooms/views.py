086b548be43b5eccccb7f422fe5638890c8d3727
KIMJUNSICK <jinaganda5@gmail.com>
2019-11-16 09:39:32 +0900

    Views: pagination with python, not django

--- rooms/views.py
+++ rooms/views.py
@@ -3,5 +3,14 @@ from . import models
 
 
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()[3:10]  # [Offset : Limit]
+    # infomation of page in request
+    # how to get page info neatly in queryset
+    #  variable for offset & limit
+    #  offset = (page-1) x 10 limit = page x 10
+    #  all_rooms = models.Room.objects.all()[offset:limit]
+    PAGE_SIZE = 10
+    page = int(request.GET.get("page", 1))
+    limit = page * PAGE_SIZE
+    offset = limit - PAGE_SIZE
+    all_rooms = models.Room.objects.all()[offset:limit]  # [Offset : Limit]
     return render(request, "rooms/home.html", context={"potato": all_rooms})
