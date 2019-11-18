d1fc9f47e990b3afe2023995153b76e3d17b3060
KIMJUNSICK <jinaganda5@gmail.com>
2019-11-18 21:41:04 +0900

    Templates: use django pagination

--- rooms/views.py
+++ rooms/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from . import models
-from math import ceil
 
 
 def all_rooms(request):
-    PAGE_SIZE = 10
-    page = request.GET.get("page", 1)
-    page = int(page or 1)  # for case that there are imcomplete page
-    limit = page * PAGE_SIZE
-    offset = limit - PAGE_SIZE
-    page_count = ceil(models.Room.objects.count() / PAGE_SIZE)
-    all_rooms = models.Room.objects.all()[offset:limit]  # [Offset : Limit]
-    return render(
-        request,
-        "rooms/home.html",
-        context={
-            "potato": all_rooms,
-            "page": page,
-            "page_count": page_count,
-            "page_range": range(1, page_count + 1),
-        },
-    )
+    page = request.GET.get("page")
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10)
+    rooms = paginator.get_page(page)
+    return render(request, "rooms/home.html", {"rooms": rooms})
