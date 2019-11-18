dde84daeefea88ddcacb9e735416d688c47adcee
KIMJUNSICK <jinaganda5@gmail.com>
2019-11-18 22:32:18 +0900

    Views: get_page vs page
    because get_page is less controllable but convenient. page vice versa.

--- rooms/views.py
+++ rooms/views.py
@@ -7,5 +7,5 @@ def all_rooms(request):
     page = request.GET.get("page", 1)
     room_list = models.Room.objects.all()
     paginator = Paginator(room_list, 10, orphans=5)
-    rooms = paginator.get_page(page)
+    rooms = paginator.page(int(page))
     return render(request, "rooms/home.html", {"page": rooms})
