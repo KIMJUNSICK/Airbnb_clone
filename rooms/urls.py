d045b57f4fcf95062e624335212437efa9ee3143
KIMJUNSICK <jinaganda5@gmail.com>
2019-11-22 00:14:48 +0900

    Views: extends filter condition with CBV

--- rooms/urls.py
+++ rooms/urls.py
@@ -8,5 +8,5 @@ app_name = "rooms"
 # and path find pk argument in ftn
 urlpatterns = [
     path("<int:pk>", views.RoomDetail.as_view(), name="detail"),
-    path("search/", views.search, name="search"),
+    path("search/", views.SearchView.as_view(), name="search"),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 from django.shortcuts import render
 from . import models, forms
 
@@ -22,19 +22,67 @@ class RoomDetail(DetailView):
     model = models.Room
 
 
-def search(request):
-    country = request.GET.get("country")
+class SearchView(View):
+    def get(self, request):
+        country = request.GET.get("country")
 
-    if country:
-        form = forms.SearchForm(
-            request.GET
-        )  # bounded form # connected to data, automatically check data
+        if country:
+            form = forms.SearchForm(
+                request.GET
+            )  # bounded form # connected to data, automatically check data
 
-        if form.is_valid():
-            print(form.cleaned_data)  # get cleaned data in request of ours
+            if form.is_valid():
+                # filtering
+                city = form.cleaned_data.get("city")
+                country = form.cleaned_data.get("country")
+                room_type = form.cleaned_data.get("room_type")
+                price = form.cleaned_data.get("price")
+                guests = form.cleaned_data.get("guests")
+                bedrooms = form.cleaned_data.get("bedrooms")
+                beds = form.cleaned_data.get("beds")
+                baths = form.cleaned_data.get("baths")
+                instant_book = form.cleaned_data.get("instant_book")
+                superhost = form.cleaned_data.get("superhost")
+                amenities = form.cleaned_data.get("amenities")
+                facilities = form.cleaned_data.get("facilities")
+                house_rules = form.cleaned_data.get("house_rules")
 
-    else:
-        form = forms.SearchForm()  # undounded form # set default value
+                filter_args = {}
 
-    return render(request, "rooms/search.html", {"form": form})
+                if city != "Anywhere":
+                    filter_args["city__startswith"] = city
+
+                filter_args["country"] = country
+
+                if room_type is not None:
+                    filter_args["room_type"] = room_type
+                if price is not None:
+                    filter_args["price__lte"] = price
+                if guests is not None:
+                    filter_args["guests__gte"] = guests
+                if bedrooms is not None:
+                    filter_args["bedrooms__gte"] = bedrooms
+                if beds is not None:
+                    filter_args["beds__gte"] = beds
+                if baths is not None:
+                    filter_args["baths__gte"] = baths
+                if instant_book is True:
+                    filter_args["instant_book"] = True
+                if superhost is True:
+                    filter_args["host__superhost"] = True
+
+                for amenity in amenities:
+                    filter_args["amenities"] = amenity
+                for facility in facilities:
+                    filter_args["facilities"] = facility
+                for house_rule in house_rules:
+                    filter_args["house_rules"] = house_rule
+
+                rooms = models.Room.objects.filter(**filter_args)
+                print(filter_args)
+                print(rooms)
+        else:
+            form = forms.SearchForm()  # undounded form # set default value
+
+        return render(request, "rooms/search.html", {"form": form, "rooms": rooms})
 
