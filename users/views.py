74e1ec245913b9655f81150f41d50e29ca6529ac
KIMJUNSICK <jinaganda5@gmail.com>
2019-11-24 21:13:55 +0900

    Auth: What is valid?

--- users/views.py
+++ users/views.py
@@ -5,12 +5,13 @@ from . import forms
 
 class LoginView(View):
     def get(self, request):
-        form = forms.LoginForm()
+        form = forms.LoginForm(initial={"email": "jinaganda5@gmail.com"})
         return render(request, "users/login.html", {"form": form})
 
     def post(self, request):
         form = forms.LoginForm(request.POST)
-        print(form)
+        print(form.is_valid())  # if form is valid, return True
+        return render(request, "users/login.html", {"form": form})
 
 
 # CSRF
