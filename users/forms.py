bcaf75ddf615a08fa861048f973431e2fbea81f1
KIMJUNSICK <jinaganda5@gmail.com>
2019-11-25 13:04:00 +0900

    Auth: signup > ModelForm clean data of model & save data in DB

--- users/forms.py
+++ users/forms.py
@@ -21,20 +21,15 @@ class LoginForm(forms.Form):
             self.add_error("email", forms.ValidationError("User does not exist"))
 
 
-class SignUpForm(forms.Form):
-    first_name = forms.CharField(max_length=80)
-    last_name = forms.CharField(max_length=80)
-    email = forms.EmailField()
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = models.User
+        fields = ("first_name", "last_name", "email")
+
     password = forms.CharField(widget=forms.PasswordInput)
     password2 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
 
-    def clean_email(self):
-        email = self.cleaned_data.get("email")
-        try:
-            models.User.objects.get(email=email)
-            raise forms.ValidationError("User already exists with that email")
-        except models.User.DoesNotExist:
-            return email
+    # email is cleaned by ModelForm
 
     def clean_password2(self):
         password = self.cleaned_data.get("password")
@@ -44,15 +39,3 @@ class SignUpForm(forms.Form):
         else:
             return password
 
-    def save(self):
-        first_name = self.cleaned_data.get("first_name")
-        last_name = self.cleaned_data.get("last_name")
-        email = self.cleaned_data.get("email")
-        password = self.cleaned_data.get("password")
-
-        # to use create_user crypt password in form
-        # create_user(username, email, password)
-        user = models.User.objects.create_user(email, email, password)
-        user.first_name = first_name
-        user.last_name = last_name
-        user.save()
