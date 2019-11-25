bb81b8b25df9452a59500e7a4c7d0aad62f2e277
KIMJUNSICK <jinaganda5@gmail.com>
2019-11-25 20:40:39 +0900

    Auth: verify email > Set Up for sending verification email + dotenv

--- config/settings.py
+++ config/settings.py
@@ -134,3 +134,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 # meaning of starting path with "/" is absolute path
 # only develope mode
 MEDIA_URL = "/media/"
+
+# Email Configuration
+
+EMAIL_HOST = "smtp.mailgun.org"
+EMAIL_PORT = "587"
+EMAIL_HOST_USER = os.environ.get("MAILGUN_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("MAILGUN_HOST_PASSWORD")
