5c09efe2221ed5da4ea35d488fc62ed1dd398e0a
KIMJUNSICK <jinaganda5@gmail.com>
2019-12-07 19:36:09 +0900

    View & Templates: user profile

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.conf import settings
 from django.core.mail import send_mail
 from django.utils.html import strip_tags
+from django.shortcuts import reverse
 from django.template.loader import render_to_string
 
 
@@ -24,7 +25,10 @@ class User(AbstractUser):
     LANGUAGE_ENGLISH = "en"
     LANGUAGE_KOREAN = "kr"
 
-    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "Korean"),
+    )
 
     CURRENCY_USD = "usd"
     CURRENCY_KRW = "krw"
@@ -42,14 +46,22 @@ class User(AbstractUser):
     )
 
     avatar = models.ImageField(upload_to="avatars", blank=True)
-    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, blank=True
+    )
     bio = models.TextField(blank=True, null=True)
     birthdate = models.DateField(blank=True, null=True)
     language = models.CharField(
-        choices=LANGUAGE_CHOICES, max_length=2, blank=True, default=LANGUAGE_KOREAN
+        choices=LANGUAGE_CHOICES,
+        max_length=2,
+        blank=True,
+        default=LANGUAGE_KOREAN,
     )
     currency = models.CharField(
-        choices=CURRENCY_CHOICES, max_length=3, blank=True, default=CURRENCY_KRW
+        choices=CURRENCY_CHOICES,
+        max_length=3,
+        blank=True,
+        default=CURRENCY_KRW,
     )
 
     superhost = models.BooleanField(default=False)
@@ -78,3 +90,7 @@ class User(AbstractUser):
             )
             self.save()
         return
+
+    # You can see user detail right away in admin with this code.
+    def get_absolute_url(self):
+        return reverse("users:profile", kwargs={"pk": self.pk})
